be6d2c5dc099bab1010d157e394b2b1928386882
lea <lea.franz@edu.fh-joanneum.at>
2020-10-18 11:42:45 +0200

    added component template

---
+++ templates/ComponentTemplate.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+const TempComponent = (props) => {};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default TempComponent;
